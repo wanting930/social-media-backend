5b669fc40c2443ff877f3cc25fcb55b31bdbebe5
Zoe <gogo97930@gmail.com>
2026-05-07 23:28:11 +0800

    Initial commit

---
+++ src/test/java/com/esun/socialmediaplatform/SocialMediaPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esun.socialmediaplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
